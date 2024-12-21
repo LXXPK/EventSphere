26eb1b34309b500e8670265371353f4fddbe775b
B_Kiran <124519398+LXXPK@users.noreply.github.com>
2024-12-21 11:33:17 +0530

    base application

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const participantRoutes = require('./routes/Participants');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log(err));
+
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use('/api/participants', participantRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
